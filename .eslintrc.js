ae4c309f747c495e0b24e02a08c08285ff3fbfe0
honglin <956866674@qq.com>
2019-05-29 18:57:14 +0800

    添加eslint忽略项

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "semi": [0], // 不检测结尾分号
+    "quotes": [0], // 不检测单双引号
+    "space-before-function-paren": [0], // 不检测函数名和括号间的空格
+    "camelcase": [0],
+    "no-trailing-spaces": [0],
+    "no-template-curly-in-string": [0]
   },
   parserOptions: {
     parser: 'babel-eslint'
