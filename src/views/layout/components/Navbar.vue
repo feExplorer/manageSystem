a418dfeb016a0a1f538b502c44cf41b02d26e4f1
honglin <956866674@qq.com>
2019-06-05 10:39:30 +0800

    添加最上方工具条

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,21 +1,103 @@
 <template>
-  <div class="navbar">
-    <el-button @click="change">open/close sidebar</el-button>
-  </div>
+  <el-menu class="navbar" mode="horizontal">
+    <hl-hamburger
+      class="hamburger-container"
+      :toggleClick="toggleSideBar"
+      :isActive="$store.state.sidebarState"
+    ></hl-hamburger>
+    <div class="right-menu">
+      <el-tooltip effect="dark" content="全屏" placement="bottom">
+        <hl-fullscreen class="right-menu-item"></hl-fullscreen>
+      </el-tooltip>
+      <el-dropdown class="avatar-container right-menu-item" trigger="click">
+        <div class="avatar-wrapper">
+          <img class="user-avatar" src="https://mgbq.github.io/nx-admin-site/home.png">
+          <i class="el-icon-caret-bottom"></i>
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <router-link to="/">
+            <el-dropdown-item>home</el-dropdown-item>
+          </router-link>
+          <a target="_blank" href="https://github.com/legendaryedu/manageSystem">
+            <el-dropdown-item>github地址</el-dropdown-item>
+          </a>
+          <el-dropdown-item divided>
+            <span @click="logout" style="display:block;">退出</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </el-menu>
 </template>
 <script>
+import hlHamburger from "@/components/hl-hamburger";
+import hlFullscreen from "@/components/hl-fullscreen";
 export default {
   name: "navbar",
+  components: {
+    hlHamburger,
+    hlFullscreen
+  },
   methods: {
-    change() {
+    toggleSideBar() {
       this.$store.state.sidebarState = !this.$store.state.sidebarState;
-    }
+    },
+    logout() {}
   }
 };
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .navbar {
   height: 50px;
+  line-height: 50px;
+  border-radius: 0px !important;
+  .hamburger-container {
+    line-height: 58px;
+    height: 50px;
+    float: left;
+    padding: 0 10px;
+    &:focus {
+      outline: none;
+    }
+  }
+  .right-menu {
+    float: right;
+    height: 100%;
+
+    .right-menu-item {
+      display: inline-block;
+      margin: 0 8px;
+    }
+    .screenfull {
+      height: 20px;
+    }
+    .international {
+      vertical-align: top;
+    }
+    .theme-switch {
+      vertical-align: 15px;
+    }
+    .avatar-container {
+      height: 50px;
+      margin-right: 30px;
+      .avatar-wrapper {
+        cursor: pointer;
+        margin-top: 5px;
+        position: relative;
+        .user-avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+        .el-icon-caret-bottom {
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
 }
 </style>
