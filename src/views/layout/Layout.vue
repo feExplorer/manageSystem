07e30fdbc8a71345e0509b2425a45f8207b66d31
honglin <956866674@qq.com>
2019-06-05 10:37:50 +0800

    去除scope

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -24,7 +24,7 @@ export default {
   }
 };
 </script>
-<style lang="less" scoped>
+<style lang="less">
 @import "../../styles/index.less";
 .app-wrapper {
   height: 100%;
@@ -44,7 +44,7 @@ export default {
 
   .main-container {
     min-height: 100%;
-    margin-left: 210px;
+    margin-left: 200px;
     transition: margin-left 0.5s;
   }
 }
